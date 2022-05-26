a5c0acd42a3cf8b9a43f362b6aff3367da2b5c5b
Prop4et <francostanco97@gmail.com>
2022-05-26 13:18:25 +0200

    added requests every 10 seconds

--- server/coap.js
+++ server/coap.js
@@ -1,9 +1,33 @@
-const coap = require("node-coap-client").CoapClient;
+const coap = require('coap')
+const parser = require('./parser')
 
-coap
-    .tryToConnect("coap://192.168.1.229")
-    .then((result) => {
-        if(result == true) console.log('coap server available');
-        else console.log('Error: ', result);
-    })
-    ;
+const topicMqtt = parser.topicMqtt;
+const subtopics = parser.subtopics;
+const server = coap.createServer()
+
+setInterval( () => {
+var req0 = coap.request('coap://192.168.1.229/'+topicMqtt+subtopics[0])
+req0.on('response', (res) => {
+    parser.parse(res.payload, subtopics[0], 'CoAP');
+})
+
+var req1 = coap.request('coap://192.168.1.229/'+topicMqtt+subtopics[1])
+req1.on('response', (res) => {
+    parser.parse(res.payload, subtopics[1], 'CoAP');
+})
+
+var req2 = coap.request('coap://192.168.1.229/'+topicMqtt+subtopics[2])
+req2.on('response', (res) => {
+    parser.parse(res.payload, subtopics[2], 'CoAP');
+})
+
+var req3 = coap.request('coap://192.168.1.229/'+topicMqtt+subtopics[3])
+req3.on('response', (res) => {
+    parser.parse(res.payload, subtopics[3], 'CoAP');
+})
+setTimeout( () => {
+req0.end();
+req1.end();
+req2.end();
+req3.end();}, 1000);
+}, 9000);
