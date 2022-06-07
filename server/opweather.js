1b159e4870ef2b2fdb37a8444a77ea06df47f882
Prop4et <francostanco97@gmail.com>
2022-06-07 10:42:00 +0200

    opweather

--- server/opweather.js
+++ server/opweather.js
@@ -1,27 +1,29 @@
 const request = require('request'); 
+const influx = require('./influx');
+const influxConfig = require('./influxconfig')
+const influxClient = new influx.InfluxClient(influxConfig.remotehost, influxConfig.port, influxConfig.token, influxConfig.org);
 var API_KEY = '70db7dbca3722a9613cc28ebb4190af7';
 
   
-let temp = 0
-const forecast = function (latitude, longitude) { 
+const forecast = (lat, lon) => { 
   
-var url = `http://api.openweathermap.org/data/2.5/weather?`
-            +`lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ lon +'&appid='+API_KEY
   
-    request({ url: url, json: true }, function (error, response) { 
+    request({ url: url, json: true }, (error, response) => { 
         if (error) { 
             console.log('Unable to connect to Forecast API'); 
         } 
-          else { 
-            console.log(JSON.stringify(response));
+        else { 
+
+            avgtemp = response.body.main.temp - 273.15
+            avghum = response.body.main.humidity
+            console.log(avgtemp.toFixed(2), avghum);
         } 
-    }) 
+    })
 } 
   
-var latitude = 44.501; // Indore latitude 
-var longitude = 11.350; // Indore longitude 
+var lat = 44.501; // Indore latitude 
+var lon = 11.350; // Indore longitude 
   
 // Function call 
-forecast(latitude, longitude); 
-
-console.log(temp)
+forecast(lat, lon); 
