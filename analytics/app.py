811a058a65b5af89b301f09d8a5098b6982f8171
Prop4et <francostanco97@gmail.com>
2022-06-08 16:56:48 +0200

    set interval for python

--- analytics/app.py
+++ analytics/app.py
@@ -6,9 +6,12 @@ import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 from datetime import timedelta
 import json
+from interval import Interval
+
 
 app = Flask(__name__)
 
+ids = []
 fd =  open('../server/influxconfig.json', 'r')
 config = json.load(fd)
 client =  InfluxDBClient(url='http://'+config["remotehost"]+':'+config["port"], token=config["token"], org=config["org"], debug=False)
@@ -17,40 +20,29 @@ fd.close()
 write_api = client.write_api(write_options=SYNCHRONOUS)
 query_api = client.query_api()  
 
-buckets = ['temp', 'hum', 'smoke']
+@app.route('/newId/<int:id>', methods=['POST'])
+def newId(id):
+    ids.append(str(id))
 
 def create_df(query):
 	result_temp = query_api.query_data_frame(query)
+	if result_temp.empty:
+		print('Query result is none')
+		return None
 	df_clean = result_temp.copy()
 	df_clean = df_clean.drop(columns=['result', 'table', '_start', '_stop', '_field', '_measurement', 'lat', 'lon', 'sensor'])
 	df_clean = df_clean.rename(columns = {'_time': 'Time', '_value': 'v'})
 	#CHECK THIS
-	df_clean['Time'] = df_clean['Time'].dt.strftime('%d-%m-%Y %H:%M:%S')
-	print(df_clean['Time'])
+	df_clean['Time'] = pd.to_datetime(df_clean['Time'].dt.strftime('%Y-%m-%d %H:%M:%S'))
+	#df_clean['Time']= pd.to_datetime(df['time'])
 	return df_clean
 
 #time(v: "2016-06-13T17:43:50.1004002Z")		
 #QUERY HANDLING
 def build_query(sensor_id, bucket):
     return 'from(bucket:"'+bucket+'")' \
-			' |> range(start: -6h)'\
-			' |> filter(fn: (r) => r._measurement == "val" and r._field == "value" and r._value > -300 and r.sensor == ''"'+str(sensor_id)+'"'')'
-
-#route to call for the training, i'd like to retrain the model daily with a span of data of 6 hours from 15 to 21, it's slow tho, i need to investigate for a good interval
-#and stop i think
-
-def get_prediction(df, window):
-	history = [x for x in df['v']]
-	predictions = list()
-	for t in range(len(df), len(df)+window):
-		model = ARIMA(history, order=(1,1,1)) #parameters obtained from the analysis of the notebook
-		model_fit = model.fit()
-		output = model_fit.forecast()
-		yest = output[0]
-		predictions.append(yest)
-		#add the value just prodced to the history, to help produce the new one
-		history.append(yest)
-	return predictions
+			' |> range(start: -2h)'\
+			' |> filter(fn: (r) => r._measurement == "val" and r._field == "value" and r.sensor == ''"'+str(sensor_id)+'"'')'
 
 def writeDB(predictions, last_time, sensor_id):
 	i = 0
@@ -58,32 +50,35 @@ def writeDB(predictions, last_time, sensor_id):
 	for p in predictions:
 		#this one needs to be printed
 		t = (last_time+timedelta(seconds=i)).strftime('%Y-%m-%dT%H:%M:%S')
-		print(t)
 		pt = Point("val").tag('sensor', sensor_id).field("value", p).time(t, write_precision=WritePrecision.S)
 		write_api.write(bucket="predictions", org="IoT", record=pt)
 		i += 1
 
 
-#route to call for the predictions for each sensor for the given time window
-#the time window for the prediction is the same foreach bucket
-@app.route('/predict/<int:sensor_id>/<int:time_window>')
-def predict(sensor_id, time_window):
-	if(time_window > 20):
-		return f'High time window'
-	for bucket in buckets:
-		query = build_query(sensor_id, bucket)
+def train_and_predict(df, window):
+    #i mean it sucks, but window something like 300
+	history = [x for x in df['v']]
+	model = ARIMA(history, order=(1,1,1))
+	fitted = model.fit()
+	return fitted.predict(start=len(df), end=len(df)+window-1)
+
+def prediction(bucket, sensorIds):
+	print(sensorIds)
+	for id in sensorIds:
+		print('id', id)
+		query = build_query(id, bucket)
+		print('query', query)
 		df = create_df(query)
-		predictions = get_prediction(df, time_window)
-		writeDB(predictions, df.iloc[len(df)-1]['time'], sensor_id)
-	return f'Predicted for sensor {sensor_id} on a time window of {time_window}'
-	
-		
-
-@app.route("/")
-def index():
-	#here you retrain the model
-	return '<b>hello world</b>'
+		if df is not None:
+			predictions = train_and_predict(df, 300)
+			writeDB(predictions, df.iloc[len(df)-1]['Time'], id)
 
 
-if __name__ == '__main__':
-    app.run()
+#need to create three of them to make it parallel
+intervalUpdateT = Interval(60.0, prediction, 'temp', ids) #retrain each model every minute
+intervalUpdateH = Interval(60.0, prediction, 'hum', ids) #retrain each model every minute
+intervalUpdateS = Interval(60.0, prediction, 'smoke', ids) #retrain each model every minute
+
+intervalUpdateT.run()
+intervalUpdateH.run()
+intervalUpdateS.run()
---
+++ analytics/interval.py
@@ -0,0 +1,32 @@
+import threading
+from typing import Callable, Any
+
+#from: https://gist.github.com/gquittet/14a824876a57e118fa8d6aaedf8224f1
+class Interval(threading.Thread):
+    """
+    This class makes the same job as setInterval in JavaScript.
+    AKA execute a function each seconds.
+    """
+    def __init__(
+            self,
+            interval: float,
+            function: Callable[[Any], Any],
+            args=None,
+            kwargs=None,
+    ) -> 'Interval':
+        threading.Thread.__init__(self)
+        self.interval = interval
+        self.function = function
+        self.args = args if args is not None else []
+        self.kwargs = kwargs if kwargs is not None else {}
+        self.event = threading.Event()
+    def cancel(self) -> None:
+        # Stop timer event
+        self.event.set()
+        # Kill thread if event is set
+        if self.event.is_set():
+            self.join()
+
+    def run(self) -> None:
+        while not (self.event.is_set() or self.event.wait(self.interval)):
+            self.function(*self.args, **self.kwargs)
