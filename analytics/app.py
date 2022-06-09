bff6fc655b8dc1a5350f361922f521b0e9cc2223
Prop4et <francostanco97@gmail.com>
2022-06-09 15:08:39 +0200

    changed config

--- analytics/app.py
+++ analytics/app.py
@@ -12,9 +12,9 @@ from interval import Interval
 app = Flask(__name__)
 
 ids = []
-fd =  open('../server/influxconfig.json', 'r')
+fd =  open('../server/config.json', 'r')
 config = json.load(fd)
-client =  InfluxDBClient(url='http://'+config["remotehost"]+':'+config["port"], token=config["token"], org=config["org"], debug=False)
+client =  InfluxDBClient(url='http://'+config["influx"]["remotehost"]+':'+config["influx"]["port"], token=config["influx"]["token"], org=config["influx"]["org"], debug=False)
 fd.close()
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
@@ -45,12 +45,11 @@ def build_query(sensor_id, bucket):
 
 def writeDB(predictions, last_time, sensor_id, bucket):
 	i = 0
-	print('last time: ', last_time)
 	for p in predictions:
     	#correct
 		t = (last_time+timedelta(seconds=i)).strftime('%Y-%m-%dT%H:%M:%S')
 		#write the new point
-		pt = Point("val").tag('sensor', sensor_id).field("bucket", bucket).field("value", p).time(t, write_precision=WritePrecision.S)
+		pt = Point("val").tag('sensor', sensor_id).tag("bucket", bucket).field("value", p).time(t, write_precision=WritePrecision.S)
 		write_api.write(bucket="predictions", org="IoT", record=pt)
 		i += 1
 
@@ -63,21 +62,21 @@ def train_and_predict(df, window):
 	return fitted.predict(start=len(df), end=len(df)+window-1)
 
 def prediction(bucket, sensorIds):
+	
 	for id in sensorIds:
-		print('id', id)
+		print('prediction for bucket ' + bucket + ' on sensor ' + str(id))
 		query = build_query(id, bucket)
-		print('query', query)
 		df = create_df(query)
 		if df is not None:
-			predictions = train_and_predict(df, 300)
+			predictions = train_and_predict(df, 70)
 			writeDB(predictions, df.iloc[len(df)-1]['Time'], id, bucket)
 
 
 #need to create three of them to make it parallel
 intervalUpdateT = Interval(60.0, prediction, ["temp", ids]) #retrain each model every minute
-intervalUpdateH = Interval(60.0, prediction, ["hum", ids]) #retrain each model every minute
-intervalUpdateS = Interval(60.0, prediction, ["smoke", ids]) #retrain each model every minute
+intervalUpdateH = Interval(60.0, prediction, ["hum", ids]) 
+intervalUpdateS = Interval(60.0, prediction, ["smoke", ids]) 
 
-intervalUpdateT.run()
-intervalUpdateH.run()
-intervalUpdateS.run()
+intervalUpdateT.start()
+intervalUpdateH.start()
+intervalUpdateS.start()
